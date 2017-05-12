79beaf68e324070744d43ab7d87fdc494eed0c8f
John Crisostomo <johncrisostomo@protonmail.ch>
2017-05-12 12:02:26 +0800

    adds error handling middleware

--- server.js
+++ server.js
@@ -4,24 +4,26 @@ const path = require('path');
 
 const app = express();
 const upload = multer({ dest: 'tmp/', 
-  limits: { fileSize: 10000000 } }).single('sample-file');
+  limits: { fileSize: 1000000 } }).single('sample-file');
 
 app.get('/', (req, res) => {
   res.set('Content-Type', 'text/html');
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.post('/api/read-metadata', (req, res) => {
-  upload(req, res, (err) => {
-    if (err) {
-      if (err.code === 'LIMIT_FILE_SIZE') {
-        res.json({ error: 'The file you are trying to upload is too large!' });
-      }
-      res.json({ error: 'Something went wrong, you are not uploading something sketchy, are you?'});
-    }
+app.post('/api/read-metadata', upload, (req, res) => {
+  res.json({ size: req.file.size });
+});
 
-    res.json({ size: req.file.size });
-  });
+app.use((err, req, res, next) => {
+  if (err) {
+    if (err.code === 'LIMIT_FILE_SIZE') {
+      res.json({ error: 'You can only upload file sizes of 500B, sorry' });
+      return;
+    } 
+
+    res.json({ error: 'Something went wrong, you are not uploading something sketchy, are you?'});
+  } 
 });
 
 app.listen(process.env.PORT || 3000);
